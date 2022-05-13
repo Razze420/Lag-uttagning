5fed412c1b332644ebe7b035afeb16f2862ccbe2
Rasmus <rasmus.ekstedt11@gmail.com>
2022-05-13 13:36:24 +0200

    finished all functions and classes

--- resources.py
+++ resources.py
@@ -1,5 +1,4 @@
 import random
-from secrets import choice
 
 class Player:
     def __init__(self, name, club, attack, defend):
@@ -20,12 +19,10 @@ class Player:
     def get_def(self):
         return self.defend
     
-    def gett_all_attributes(self):
+    def get_all_attributes(self):
         return self.name, self.club, self.attack, self.defend
 
-#    def alternative_players(self):
-#        players = []
-#        players.append(Players(""))
+# We have created a player class were we have used attack, defend, name and club.
 
 
 class Team:
@@ -41,10 +38,13 @@ class Team:
     def __str__(self):
         return f"Name: {self.name}, Attack: {self.combat_point}, Defense: {self.defense_point}, P1: {self.players[0].name}, P2: {self.players[1].name}, P3: {self.players[2].name}"
 
+    def get_all_attributes(self):
+        return self.name, self.players, self.combat_point, self.defense_point
+
     
 def load_players():
 
-    with open("Slutprojekt/players.txt", "r" , encoding="utf8") as f:
+    with open("players.txt", "r" , encoding="utf8") as f:
         players = []
         for line in f.readlines():
             attributes = line.split("/")
@@ -54,6 +54,8 @@ def load_players():
                                   int(attributes[3]))
             players.append(this_player)
         return players
+    """_summary_ 
+    """
 
 
 def create_player(available_players : list):
@@ -68,28 +70,15 @@ def create_player(available_players : list):
             print(j, player.name)
         choice = input("Pick an index: ")
         picked_players.append(available_players.pop(int(choice)))
+    #This loop works as every time the user picks a player the players is added in a list and is taken away from the list with every on else
         
 
     print("===== YOUR TEAM =====")
     for player in picked_players:
         print(player)
+    #Here i just print every player the user have picked
     
     return picked_players
-
-    # name = input("name: ")
-    # while True:
-    #     if name == "Bahoui" or "J.Larsson" or "Otieno":
-    #         club = "AIK"
-    #         True
-    #     elif name == "Selmani" or "Besara" or "jehaze":
-    #         club = "HIF"
-    #         True
-    #     elif name == "Edvardsen" or "Shuller" or "Ekdal":
-    #         club = "DIF"
-    #         True
-    #     else:
-    #         print("Din dumma jävel välj en av spelarna\nOBS!Glöm inte stor bokstav!")
-    #         False
     
 def load_opponents(available_players : list):
     opponents = []
@@ -105,8 +94,16 @@ def load_team(picked_players):
 
     return Team("ditt lag", team)  
 
-def fight():
-    pass
+def fight(team: Team, opponents: Team):
+    tot_stats_team = team.combat_point + team.defense_point
+    tot_stats_opponents = opponents.combat_point + opponents.defense_point
+    if tot_stats_team < tot_stats_opponents:
+        return "You lost to ", Team(opponents)
+    elif tot_stats_team > tot_stats_opponents:
+        return "You won!!\nCongrats!"
+    elif tot_stats_team == tot_stats_opponents:
+        return "There was unfortunantly no winner:("
+
 
 def main():
     available_players = load_players()
@@ -117,11 +114,14 @@ def main():
     print("\n\n\n")
     
     team = load_team(available_players)
-    print(team"\n")
+    print(team)
+    print("\n")
 
     opponents = load_opponents(available_players)
     print(opponents)
-    
+
+    results = fight(team, opponents)
+    print(results)
 
 
 if __name__ == "__main__":
